2a09ad53214d48ce269f20079dab223328db0207
meain <meain@users.noreply.github.com>
2022-03-04 13:08:13 +0000

    Update tree-sitter queries to latest

--- queries/java/textobjects.scm
+++ queries/java/textobjects.scm
@@ -45,6 +45,18 @@
 (formal_parameters
   . (formal_parameter) @parameter.inner
   . ","? @_end
- (#make-range! "parameter.outer" @_start @parameter.inner))
+ (#make-range! "parameter.outer" @parameter.inner @_end))
 
-(comment) @comment.outer
+(argument_list
+  "," @_start .
+  (_) @parameter.inner
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+(argument_list
+  . (_) @parameter.inner
+  . ","? @_end
+ (#make-range! "parameter.outer" @parameter.inner @_end))
+
+[
+  (line_comment)
+  (block_comment)
+] @comment.outer
--- queries/latex/textobjects.scm
+++ queries/latex/textobjects.scm
@@ -1,13 +1,14 @@
-((environment
+((generic_environment
   . (_)
   . (_) @_start
   (_) @_end . (_) .
 ) @block.outer
 (#make-range! "block.inner" @_start @_end))
 
-((environment
+((generic_environment
   (begin
-   name: (word) @_frame)
+   name: (curly_group_text
+           (text) @_frame))
   . (_) @_start
   (_) @_end . (_) .) @frame.outer
  (#eq? @_frame "frame")
