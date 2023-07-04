08e87eac287ae466b5f3f4d2136bef8a3038f452
Abin Simon <mail@meain.io>
2023-07-04 20:10:55 +0530

    Update grammars

---
+++ queries/glimmer/textobjects.scm
@@ -0,0 +1,29 @@
+[
+ (element_node)
+ (block_statement)
+] @function.outer
+
+ [
+   (mustache_statement)
+   (block_statement_start)
+ ] @block.outer
+
+(attribute_node) @attribute.outer
+(attribute_node
+  [
+   (concat_statement)
+   (mustache_statement)
+  ] @attribute.inner)
+
+(element_node (element_node_start) . (_) @function.inner . (element_node_end))
+(block_statement (block_statement_start) . (_) @function.inner . (block_statement_end))
+
+((element_node (element_node_start) . (_)  @function.inner._start (_)  @function.inner._end . (element_node_end))
+ )
+((block_statement (block_statement_start) . (_)  @function.inner._start (_)  @function.inner._end . (block_statement_end))
+ )
+(mustache_statement . "{{" (_) @block.inner . "}}")
+(block_statement_start . "{{#" (_) @block.inner . "}}")
+
+(comment_statement) @comment.outer
+
